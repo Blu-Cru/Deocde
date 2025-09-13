6434b7f15bcc651444f308e885883fa477d4207d
Deven Balaji <devenvbalaji@gmail.com>
2025-09-13 12:39:58 -0400

    started pure pursuit computer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/sixWheelDrive/purePursuit/PurePursuitComputer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/sixWheelDrive/purePursuit/PurePursuitComputer.java
@@ -11,4 +11,17 @@ public class PurePursuitComputer {
         this.lookAheadDist = lookAheadDist;
     }
 
+    public void getLineIntersections(double[] p1, double[] p2, Pose2d robotPose){
+
+        double[] p1Shifted = {p1[0]-robotPose.getX(), p1[1]-robotPose.getY()};
+        double[] p2Shifted = {p2[0] - robotPose.getX(), p2[1] - robotPose.getY()};
+        //robot pose is now 0,0,h, and because heading doesnt matter for line intersections, the robot is equivalently at 0,0
+
+        double dx = p2[0] - p1[0];
+        double dy = p2[1] - p1[1];
+
+        double dr = Math.sqrt(dx*dx + dy*dy);
+
+    }
+
 }
