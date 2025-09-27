250a93f6d9c3aaf17dad314a15899973b47b705e
Deven Balaji <devenvbalaji@gmail.com>
2025-09-26 22:57:15 -0400

    progress on 6wd class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/sixWheelDrive/SixWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/sixWheelDrive/SixWheelDrive.java
@@ -1,20 +1,42 @@
 package org.firstinspires.ftc.teamcode.blucru.common.subsytems.sixWheelDrive;
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.blucru.common.hardware.motor.BluMotor;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.BluSubsystem;
+import org.firstinspires.ftc.teamcode.blucru.common.subsytems.sixWheelDrive.localization.RobotLocalizer;
+import org.firstinspires.ftc.teamcode.blucru.common.subsytems.sixWheelDrive.localization.Pinpoint;
 import org.firstinspires.ftc.teamcode.blucru.common.util.Globals;
+import org.firstinspires.ftc.teamcode.blucru.common.util.Pose2d;
 
 public class SixWheelDrive implements BluSubsystem {
 
     BluMotor[] dtMotors;
 
-    public SixWheelDrive(BluMotor fl, BluMotor fr, BluMotor bl, BluMotor br){
+    RobotLocalizer localizer;
+
+    public enum State{
+        IDLE,
+        PID,
+        TELE_DRIVE
+    }
+
+    State dtState;
+
+    public SixWheelDrive(){
+        this(new BluMotor(Globals.flMotorName),
+                new BluMotor(Globals.flMotorName),
+                new BluMotor(Globals.blMotorName),
+                new BluMotor(Globals.brMotorName));
+    }
+    private SixWheelDrive(BluMotor fl, BluMotor fr, BluMotor bl, BluMotor br){
         dtMotors = new BluMotor[]{fl, fr, bl, br};
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
+        localizer = new Pinpoint("pinpoint");
+        dtState = State.IDLE;
     }
 
     @Override
@@ -29,6 +51,7 @@ public class SixWheelDrive implements BluSubsystem {
         for (BluMotor motors: dtMotors){
             motors.read();
         }
+        localizer.read();
     }
 
     @Override
@@ -46,6 +69,31 @@ public class SixWheelDrive implements BluSubsystem {
         }
     }
 
+    public void teleDrive(Gamepad g1, double tol){
+        double x = -g1.left_stick_y;
+        double r = g1.left_stick_x;
+
+        if (Math.abs(x) <= tol){
+            x = 0;
+        }
+        if (Math.abs(r) <= tol){
+            r = 0;
+        }
+
+        if (x == 0 && r == 0){
+            if (dtState == State.PID){
+                //in pid
+            } else {
+                //either stopped driving or idle alr
+                dtState = State.IDLE;
+            }
+        } else {
+            dtState = State.TELE_DRIVE;
+            drive(x,r);
+        }
+
+    }
+
     @Override
     public void telemetry(Telemetry telemetry) {
         for (BluMotor motor:dtMotors){
@@ -55,6 +103,6 @@ public class SixWheelDrive implements BluSubsystem {
 
     @Override
     public void reset() {
-
+        localizer.setPosition(new Pose2d(0,0,0));
     }
 }
