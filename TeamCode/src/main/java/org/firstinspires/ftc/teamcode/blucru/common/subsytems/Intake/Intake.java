ff86702bf1a5b91439b85759879e24adcc4cbf30
InutilePleb <michael.dong.haolin@gmail.com>
2025-10-01 17:53:24 -0400

    Completed Intake Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/Intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/Intake/Intake.java
@@ -11,20 +11,30 @@ import org.firstinspires.ftc.teamcode.blucru.common.subsytems.BluSubsystem;
 
 public class Intake implements BluSubsystem {
     private BluMotor motor;
+    private static final double JAM_CURRENT_THRESHOLD = 2500; // milliamps, adjust as needed
     public enum State{
         IN,
         OUT,
         IDlE
     }
     public State state;
+
+    public Intake(String motorName) {
+        motor = new BluMotor(motorName);
+        state = State.IDlE;
+    }
+
     @Override
     public void init() {
-
+        motor.init();
     }
 
     @Override
     public void read() {
-
+        motor.read();
+        if (state == State.IN && motor.getCurrent() > JAM_CURRENT_THRESHOLD) {
+            state = State.OUT; // Jam detected, spit out the ball
+        }
     }
 
     @Override
@@ -40,15 +50,17 @@ public class Intake implements BluSubsystem {
                 motor.setPower(0);
                 break;
         }
+        motor.write();
     }
 
     @Override
     public void telemetry(Telemetry telemetry) {
-
+        motor.telemetry();
     }
 
     @Override
     public void reset() {
-
+        motor.setPower(0);
+        motor.write();
     }
 }
