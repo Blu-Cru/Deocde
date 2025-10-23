0a82c5f79d5060629575b1a3bb6de63a78b9b00e
Deven Balaji <devenvbalaji@gmail.com>
2025-10-22 20:00:21 -0400

    ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/turret/turretCommands/TurnTurretToPosCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/turret/turretCommands/TurnTurretToPosCommand.java
@@ -10,7 +10,7 @@ public class TurnTurretToPosCommand extends InstantCommand {
         super (() -> {
             Robot.getInstance().turret.setAngle(pos);
         });
-
+        
         addRequirements(Robot.getInstance().turret);
     }
 
