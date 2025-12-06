360fda494e7b30bd5e10c0f04098bf1ebd8c18ed
Michael <michael.dong.haolin@gmail.com>
2025-12-06 13:58:27 -0500

    auto opmode creation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/auto/NoPartnerFarGateAutoNoPreload.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.blucru.opmodes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.blucru.opmodes.BluLinearOpMode;
+
+@Autonomous(name = "Far Gate Auto No Partner Preload", group = "auto")
+public class NoPartnerFarGateAutoNoPreload extends BluLinearOpMode {
+    // TODO: Add trajectory sequence when rr package is configured
+}
