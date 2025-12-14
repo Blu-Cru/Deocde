2e15d676e5b6f219ceca19d6216d1079f7c86466
CooperLi1 <112364250+CooperLi1@users.noreply.github.com>
2025-12-14 00:59:02 -0500

    far shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/shooter/ShooterAutoAimInterpolation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/shooter/ShooterAutoAimInterpolation.java
@@ -5,14 +5,14 @@ import com.qualcomm.robotcore.util.Range;
 public class ShooterAutoAimInterpolation {
 
     private static final double[] dists = {
-            27.5, 33.0, 36.75, 42.0, 49.5, 55.25, 62.0, 72.5, 83.0, 93.5
+            27.5, 33.0, 36.75, 42.0, 49.5, 55.25, 62.0, 72.5, 83.0, 93.5, 113.5
     };
 
     // Arrays populated with your data
-    private static final double[] leftAngles = {26, 26, 32, 34, 39, 41, 42, 42, 45, 46};
-    private static final double[] middleAngles = {28, 28, 42, 44, 47, 48, 49, 50, 50, 50};
-    private static final double[] rightAngles = {26, 26, 36, 38, 43, 45, 46, 47, 48, 50};
-    private static final double[] velocities = {850, 850, 900, 1000, 1030, 1070, 1100, 1150, 1170, 1230};
+    private static final double[] leftAngles = {26, 26, 32, 34, 39, 41, 42, 42, 45, 46, 50};
+    private static final double[] middleAngles = {28, 28, 42, 44, 47, 48, 49, 50, 50, 50, 50};
+    private static final double[] rightAngles = {26, 26, 36, 38, 43, 45, 46, 47, 48, 50, 50};
+    private static final double[] velocities = {850, 850, 900, 1000, 1030, 1070, 1100, 1150, 1170, 1230, 1300};
 
     public static double[] interpolate(double dist){
         // clamp to range to prevent hardware damage or crashes
