37fb51460b74045821fa75c40eb14f9b96238930
Chase Liu <120326958+chase-m-liu@users.noreply.github.com>
2025-12-28 01:11:50 -0500

    fix pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/turret/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/turret/Turret.java
@@ -6,16 +6,10 @@ import com.arcrobotics.ftclib.controller.PIDController;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.blucru.common.hardware.motor.BluEncoder;
-import org.firstinspires.ftc.teamcode.blucru.common.hardware.motor.BluMotor;
-import org.firstinspires.ftc.teamcode.blucru.common.hardware.motor.BluMotorWithEncoder;
 import org.firstinspires.ftc.teamcode.blucru.common.hardware.servo.BluCRServo;
-import org.firstinspires.ftc.teamcode.blucru.common.hardware.servo.BluPIDServo;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.BluSubsystem;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.Robot;
-import org.firstinspires.ftc.teamcode.blucru.common.util.Alliance;
 import org.firstinspires.ftc.teamcode.blucru.common.util.Globals;
-import org.firstinspires.ftc.teamcode.blucru.common.util.MotionProfile;
-import org.firstinspires.ftc.teamcode.blucru.common.util.PDController;
 import org.firstinspires.ftc.teamcode.blucru.common.util.Pose2d;
 import org.firstinspires.ftc.teamcode.blucru.common.util.Vector2d;
 import com.qualcomm.robotcore.util.Range;
@@ -24,32 +18,37 @@ import com.qualcomm.robotcore.util.Range;
 public class Turret implements BluSubsystem, Subsystem {
     private TurretServos servos;
     private BluEncoder encoder;
-    private PDController nearController;
-    private PDController farController;
-    private double position;
-    private final double TICKS_PER_REV = 8192 * 212.0/35;
-    Pose2d goalPose;
-    public static double farP = 0.06, farD = 0;
-    public static double closeP = 0.07, closeD = 0.05;
-    public static double farVsCloseCutoff = 10;
-    public static double acceptableError = 0;
-    public static double powerClip = 0.8;
-    public static double MAX_ANGLE = 10;
-    public static double MIN_ANGLE = -10;
-    public static double distFromCenter = 72.35/25.4;
+    private PIDController controller;
 
-    private enum State{
+    private double position;
+    private final double TICKS_PER_REV = 8192 * 212.0 / 35;
+
+    // Static variables for Dashboard tuning
+    public static double kP = 0.08;
+    public static double kI = 0.035;
+    public static double kD = 0.007;
+
+    public static double acceptableError = 0.5;
+    public static double powerClip = 1;
+
+    // Fixed limits to allow 90 degrees
+    public static double MAX_ANGLE = 110;
+    public static double MIN_ANGLE = -110;
+
+    public static double distFromCenter = 72.35 / 25.4;
+
+    private enum State {
         MANUAL,
         PID,
         LOCK_ON_GOAL
     }
+
     private State state;
 
-    public Turret(BluCRServo servoLeft, BluCRServo servoRight, BluEncoder encoder){
+    public Turret(BluCRServo servoLeft, BluCRServo servoRight, BluEncoder encoder) {
         servos = new TurretServos(servoLeft, servoRight);
         this.encoder = encoder;
-        nearController = new PDController(closeP, closeD);
-        farController = new PDController(farP, farD);
+        controller = new PIDController(kP, kI, kD);
         state = State.MANUAL;
     }
 
@@ -67,8 +66,7 @@ public class Turret implements BluSubsystem, Subsystem {
 
     @Override
     public void write() {
-
-        switch(state){
+        switch (state) {
             case MANUAL:
                 break;
             case LOCK_ON_GOAL:
@@ -81,140 +79,110 @@ public class Turret implements BluSubsystem, Subsystem {
             case PID:
                 updateControlLoop();
                 break;
-
         }
-
         servos.write();
         encoder.write();
     }
 
-    public void setAngle(double angle){
+    public void setAngle(double angle) {
         setAngle(angle, true);
     }
-    public void setAngle(double angle, boolean switchState){
+
+    public void setAngle(double angle, boolean switchState) {
         this.position = angle;
-        if (switchState){
+        if (switchState) {
             state = State.PID;
         }
     }
 
-    public void setPower(double power){
+    public void setPower(double power) {
         servos.setPower(power);
         state = State.MANUAL;
     }
 
-    public void setFieldCentricPosition(double position, double robotHeading, boolean switchState){
+    public void setFieldCentricPosition(double position, double robotHeading, boolean switchState) {
         setAngle(180 - position - robotHeading, switchState);
     }
 
-    public void lockOnGoal(){
+    public void lockOnGoal() {
         state = State.LOCK_ON_GOAL;
     }
 
-    public void toggleManual(){
+    public void toggleManual() {
         state = State.MANUAL;
     }
 
-    public boolean isManual(){
+    public boolean isManual() {
         return state == State.MANUAL;
     }
 
-    public double getRotateError(double currAngle, double targetAngle){
-
-        return targetAngle-currAngle;
+    public double getAngle() {
+        return encoder.getCurrentPos() * (360.0 / TICKS_PER_REV);
     }
 
-    public double getEncoderPos(){
-        return encoder.getCurrentPos();
-    }
-
-    public double getPower(){return servos.getPower();}
-
-    public double getAngle(){
-        return encoder.getCurrentPos() * (360 / TICKS_PER_REV);
-    }
-
-    public void updatePD(){
-        nearController.setPD(farP,farD);
-        farController.setPD(closeP, closeD);
+    public void updatePID() {
+        controller.setPID(kP, kI, kD);
     }
 
     public void resetEncoder() {
         encoder.reset();
     }
 
-    public void updateControlLoop(){
-        while (position > 180){
-            position -= 360;
-        }
-        while (position <= -180){
-            position += 360;
-        }
-        this.position = Range.clip(this.position, MIN_ANGLE, MAX_ANGLE);
+    public double getEncoderPos() {
+        return encoder.getCurrentPos();
+    }
+
+    public void updateControlLoop() {
+        // Essential: Sync Dashboard values to the controller
+        updatePID();
+
+        while (position > 180) position -= 360;
+        while (position <= -180) position += 360;
+
+        position = Range.clip(position, MIN_ANGLE, MAX_ANGLE);
+
         double currentAngle = getAngle();
-        double rotateError = getRotateError(getAngle(), position);
-        double power;
-        if (Math.abs(rotateError) < farVsCloseCutoff){
-            Globals.telemetry.addLine("CLOSE SETTINGS");
-            power = nearController.calculate(rotateError, 0);
-        } else {
-            Globals.telemetry.addLine("FAR SETTINGS");
-            power = farController.calculate(rotateError, 0);
-        }
+        double error = position - currentAngle;
+
+        // Calculate power (FTCLib calculate is target - current)
+        double power = controller.calculate(currentAngle, position);
         power = Range.clip(power, -powerClip, powerClip);
 
-        if (currentAngle > MAX_ANGLE+3 && power > 0) {
-            power = 0;
-        }
-        else if (currentAngle < MIN_ANGLE-3 && power < 0) {
-            power = 0;
-        }
+        // Software limits safety
+        if (currentAngle > MAX_ANGLE + 3 && power > 0) power = 0;
+        if (currentAngle < MIN_ANGLE - 3 && power < 0) power = 0;
 
         Globals.telemetry.addData("Calculated Power", power);
-        Globals.telemetry.addData("Position", position);
-        if (Math.abs(rotateError) > acceptableError) {
-            Globals.telemetry.addData("Rotate Error", rotateError);
+        Globals.telemetry.addData("Target", position);
+        Globals.telemetry.addData("Error", error);
+
+        if (Math.abs(error) > acceptableError) {
             servos.setPower(power);
         } else {
-            Globals.telemetry.addLine("At Set Point");
             servos.setPower(0);
         }
     }
 
-    public double getFieldCentricTargetGoalAngle(Pose2d robotPose){
-        Vector2d target = Globals.mapVector(
-                Globals.shootingGoalLPose.getX(),
-                Globals.shootingGoalLPose.getY()
-        );
+    public double getFieldCentricTargetGoalAngle(Pose2d robotPose) {
+        Vector2d target = Globals.mapVector(Globals.shootingGoalLPose.getX(), Globals.shootingGoalLPose.getY());
         Vector2d robotVec = robotPose.vec();
-
-        // robot heading in field frame
-        double robotHeadingDeg =
-                Math.toDegrees(robotPose.getH());
-
-        // turret center in field frame
+        double robotHeadingDeg = Math.toDegrees(robotPose.getH());
         double turretCenterX = robotVec.getX() - distFromCenter * Math.cos(Math.toRadians(robotHeadingDeg));
         double turretCenterY = robotVec.getY() - distFromCenter * Math.sin(Math.toRadians(robotHeadingDeg));
-        // vector from robot to target
         double dx = target.getX() - turretCenterX;
-        Globals.telemetry.addData("dx", dx);
         double dy = target.getY() - turretCenterY;
-        Globals.telemetry.addData("dy", dy);
-
-        // absolute field angle to target
-        return Math.toDegrees(Math.atan(Math.abs(dy/dx))) + 90;
+        return Math.toDegrees(Math.atan2(dy, dx)) + 90;
     }
-
 
     @Override
     public void telemetry(Telemetry telemetry) {
         servos.telemetry();
         encoder.telemetry();
         telemetry.addData("Turret State", state);
+        telemetry.addData("Turret Angle", getAngle());
+        telemetry.addData("Turret Target", position);
     }
 
     @Override
-    public void reset() {
-//        encoder.reset();
-    }
+    public void reset() {}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/test/TurretPIDTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/test/TurretPIDTuning.java
@@ -7,38 +7,50 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.blucru.common.util.Globals;
 import org.firstinspires.ftc.teamcode.blucru.opmodes.BluLinearOpMode;
+
 @Config
 @TeleOp(group = "test")
-
 public class TurretPIDTuning extends BluLinearOpMode {
 
+    // Dashboard tunables
     public static double angle = 0;
     public static double power = 0;
-    public static double state = 0;
 
+    public static int state = 0;
 
     @Override
     public void initialize() {
         super.reportTelemetry = true;
         robot.clear();
         addTurret();
-        Globals.multiTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        Globals.multiTelemetry = new MultipleTelemetry(
+                telemetry,
+                FtcDashboard.getInstance().getTelemetry()
+        );
     }
 
-    public void periodic(){
+    @Override
+    public void periodic() {
+
+        // Live PID tuning
+        turret.updatePID();
+
         if (state == 0) {
             turret.setAngle(angle);
-
-            turret.updatePD();
-        }else{
+        } else {
             turret.setPower(power);
         }
 
-        //Globals.multiTelemetry.addData("Turret Pos", turret.getAngle());
-        //Globals.multiTelemetry.addData("Target Pos", angle);
-        //Globals.multiTelemetry.addData("Target Power", turret.getPower());
-        //Globals.multiTelemetry.addData("Error", turret.getRotateError(turret.getAngle(), angle));
+        double currentAngle = turret.getAngle();
+        double error = angle - currentAngle;
+
+        Globals.multiTelemetry.addData("Turret Angle", currentAngle);
+        Globals.multiTelemetry.addData("Target Angle", angle);
+        Globals.multiTelemetry.addData("Error", error);
+        Globals.multiTelemetry.addData("Mode", state == 0 ? "PID" : "MANUAL");
+
         turret.telemetry(telemetry);
-        //Globals.multiTelemetry.update();
+        Globals.multiTelemetry.update();
     }
 }
