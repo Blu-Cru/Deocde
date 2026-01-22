1105ae8b3c57971e83cfea2533c49702773b739b
Deven Balaji <devenvbalaji@gmail.com>
2026-01-21 19:03:17 -0500

    elevator poses tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/elevator/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/elevator/Elevator.java
@@ -11,13 +11,13 @@ public class Elevator implements BluSubsystem, Subsystem {
     private BluServo elevatorServoLeft;
     private BluServo elevatorServoRight;
     private BluBrushlandLabsColorSensor leftSensor, middleSensor, rightSensor;
-    private static final double DOWN_POSITION_LEFT = 0.01;//TODO: find positions
-    private static final double UP_POSITION_LEFT = 0.3;
-    private static final double MIDDLE_POSITION_LEFT = 0.13;
+    private static final double DOWN_POSITION_LEFT = 0.65;//TODO: find positions
+    private static final double UP_POSITION_LEFT = 0.35;
+    private static final double MIDDLE_POSITION_LEFT = 0.62;
 
-    private static final double DOWN_POSITION_RIGHT = 0.99;//TODO: find positions
-    private static final double UP_POSITION_RIGHT = 0.7;
-    private static final double MIDDLE_POSITION_RIGHT = 0.87;
+    private static final double DOWN_POSITION_RIGHT = 0.35;//TODO: find positions
+    private static final double UP_POSITION_RIGHT = 0.65;
+    private static final double MIDDLE_POSITION_RIGHT = 0.38;
 
     public Elevator(){
         elevatorServoLeft = new BluServo("elevatorLeft");
