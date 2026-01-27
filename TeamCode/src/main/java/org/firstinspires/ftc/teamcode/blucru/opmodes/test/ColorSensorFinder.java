7e7baee30e8f9f0727a3636016690701a31936ca
Michael <michael.dong.haolin@gmail.com>
2026-01-27 17:04:34 -0500

    made program to test all color sensors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/test/ColorSensorFinder.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.blucru.opmodes.test;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+@TeleOp
+public class ColorSensorFinder extends LinearOpMode {
+
+    RevColorSensorV3 sensor1;
+    RevColorSensorV3 sensor2;
+    RevColorSensorV3 sensor3;
+    RevColorSensorV3 sensor4;
+    RevColorSensorV3 sensor5;
+    RevColorSensorV3 sensor6;
+
+    public void runOpMode() {
+        sensor1 = hardwareMap.tryGet(RevColorSensorV3.class, "colorSensor1");
+        sensor2 = hardwareMap.tryGet(RevColorSensorV3.class, "colorSensor2");
+        sensor3 = hardwareMap.tryGet(RevColorSensorV3.class, "colorSensor3");
+        sensor4 = hardwareMap.tryGet(RevColorSensorV3.class, "colorSensor4");
+        sensor5 = hardwareMap.tryGet(RevColorSensorV3.class, "colorSensor5");
+        sensor6 = hardwareMap.tryGet(RevColorSensorV3.class, "colorSensor6");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            displaySensorData("Sensor 1", sensor1);
+            displaySensorData("Sensor 2", sensor2);
+            displaySensorData("Sensor 3", sensor3);
+            displaySensorData("Sensor 4", sensor4);
+            displaySensorData("Sensor 5", sensor5);
+            displaySensorData("Sensor 6", sensor6);
+            telemetry.update();
+        }
+    }
+
+    private void displaySensorData(String name, RevColorSensorV3 sensor) {
+        if (sensor == null) {
+            telemetry.addData(name, "NOT CONNECTED");
+        } else {
+            NormalizedRGBA colors = sensor.getNormalizedColors();
+            telemetry.addData(name + " R", "%.3f", colors.red);
+            telemetry.addData(name + " G", "%.3f", colors.green);
+            telemetry.addData(name + " B", "%.3f", colors.blue);
+        }
+    }
+
+}
