3c824aad2d1f37850eaec7be6496bb2c85068103
Deven Balaji <devenvbalaji@gmail.com>
2026-01-27 13:13:58 -0500

    forgot @teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/test/ThresholdFinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/test/ThresholdFinder.java
@@ -2,8 +2,9 @@ package org.firstinspires.ftc.teamcode.blucru.opmodes.test;
 
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
-
+@TeleOp
 public class ThresholdFinder extends LinearOpMode {
 
     RevColorSensorV3 sensor;
