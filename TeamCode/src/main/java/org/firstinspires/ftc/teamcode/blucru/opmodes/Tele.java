800e95fa8699780909668be451e5e7c42cb36ae4
CooperLi1 <112364250+CooperLi1@users.noreply.github.com>
2026-01-30 00:47:37 -0500

    shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/Tele.java
@@ -121,24 +121,33 @@ public class Tele extends BluLinearOpMode{
                 })
                 .transition(() -> driver1.pressedDpadLeft(), State.DRIVING_TO_SHOOT, () -> {
                     gamepad1.rumble(rumbleDur);
-                    shot+=1;
-                    new SequentialCommandGroup(
-                            new LeftTransferUpCommand()
+                    new ConditionalCommand(
+                            new ShootBallsCommand(),
+                            new LeftTransferUpCommand(),
+                            () -> (shot == 2)
                     ).schedule();
+                    shot+=1;
                 })
                 .transition(() -> driver1.pressedDpadUp(), State.DRIVING_TO_SHOOT, () -> {
                     gamepad1.rumble(rumbleDur);
-                    shot+=1;
-                    new SequentialCommandGroup(
-                            new MiddleTransferUpCommand()
+                    new ConditionalCommand(
+                            new ShootBallsCommand(),
+                            new MiddleTransferUpCommand(),
+                            () -> (shot == 2)
                     ).schedule();
+                    shot+=1;
                 })
                 .transition(() -> driver1.pressedDpadRight(), State.DRIVING_TO_SHOOT, () -> {
                     gamepad1.rumble(rumbleDur);
-                    shot+=1;
-                    new SequentialCommandGroup(
-                            new RightTransferUpCommand()
+                    new ConditionalCommand(
+                            new ShootBallsCommand(),
+                            new RightTransferUpCommand(),
+                            () -> (shot == 2)
                     ).schedule();
+                    shot+=1;
+                })
+                .transition(() -> shot >= 3, State.INTAKING, () -> {
+                    shot = 0;
                 })
 
                 .state(State.INTAKING_FROM_ABOVE)
