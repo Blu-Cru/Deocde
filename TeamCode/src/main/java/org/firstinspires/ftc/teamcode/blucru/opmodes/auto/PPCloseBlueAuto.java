821222898b5ad08e020e1bf4f6f205bc9f3bf23d
Michael <michael.dong.haolin@gmail.com>
2026-01-31 22:05:29 -0500

    close blue working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/auto/PPCloseBlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/auto/PPCloseBlueAuto.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.blucru.opmodes.auto;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.blucru.common.commands.autonomousCommands.AutonomousShootAntiJamCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.commands.autonomousCommands.AutonomousShootCommand;
@@ -12,17 +11,15 @@ import org.firstinspires.ftc.teamcode.blucru.common.pathing.Path;
 import org.firstinspires.ftc.teamcode.blucru.common.pathing.SixWheelPIDPathBuilder;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.intake.IntakeSpitCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.shooter.shooterCommands.SetShooterVelocityIndependentCommand;
-import org.firstinspires.ftc.teamcode.blucru.common.subsytems.turret.turretCommands.LockOnGoalCommand;
-import org.firstinspires.ftc.teamcode.blucru.common.subsytems.turret.turretCommands.TurnTurretToPosCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.turret.turretCommands.TurnTurretToPosFieldCentricCommand;
 import org.firstinspires.ftc.teamcode.blucru.common.util.Point2d;
 import org.firstinspires.ftc.teamcode.blucru.common.util.Pose2d;
 import org.firstinspires.ftc.teamcode.blucru.opmodes.BluLinearOpMode;
 
 @Autonomous
-public class PPCloseRedAuto extends BluLinearOpMode {
-    double turretAngle = 217; //field centric, decrease = towards obelisk increase = towards gate
-    double velo =1120;
+public class PPCloseBlueAuto extends BluLinearOpMode {
+    double turretAngle = 137; //field centric, decrease = more towards gate, increase = towards obelisk
+    double velo =1150;
     double leftHood;
     double middleHood ;
     double rightHood ;
@@ -33,11 +30,10 @@ public class PPCloseRedAuto extends BluLinearOpMode {
         public TestingPath() {
             super();
 
-            // Shift applied: dx = -6, dy = +2 (old start -45,52 -> new start -51,54)
 
             this.addPurePursuitPath(new Point2d[]{
-                            new Point2d(-51, 54),   // was (-45, 52)
-                            new Point2d(-16, 19)    // was (-10, 17)
+                            new Point2d(-49, -54),   // was (-45, 52)
+                            new Point2d(-16, -19)    // was (-10, 17)
                     }, 5000)
                     .waitMilliseconds(500)
                     // SHOOT PRELOAD
@@ -49,12 +45,12 @@ public class PPCloseRedAuto extends BluLinearOpMode {
 
                     // INTAKE FIRST SET
                     .waitMilliseconds(200)
-                    .addTurnTo(90, 5000)
+                    .addTurnTo(-90, 5000)
                     .addPurePursuitPath(new Point2d[]{
-                            new Point2d(-16, 19),
-                            new Point2d(-16,33),
-                            new Point2d(-6,45),
-                            new Point2d(-5, 57)
+                            new Point2d(-16, -19),
+                            new Point2d(-16,-33),
+                            new Point2d(-6,-45),
+                            new Point2d(-5, -57)
                     }, 2000)
 
                     .callback(() -> {
@@ -67,10 +63,10 @@ public class PPCloseRedAuto extends BluLinearOpMode {
 
                     // HEAD BACK
                     .addPurePursuitPath(new Point2d[]{
-                            new Point2d(-5, 54),   // was (-10, 50)
-                            new Point2d(-16, 19)    // was (-10, 17)
+                            new Point2d(-5, -54),   // was (-10, 50)
+                            new Point2d(-16, -19)    // was (-10, 17)
                     }, 2000)
-                    .addTurnTo(45,1000)
+                    .addTurnTo(-45,1000)
                     .waitMilliseconds(500)
                     .callback(()->{
                         new TurnTurretToPosFieldCentricCommand(turretAngle).schedule();
@@ -87,10 +83,10 @@ public class PPCloseRedAuto extends BluLinearOpMode {
                     // INTAKE SECOND SET
                     //.addTurnTo(70, 2000)
                     .addPurePursuitPath(new Point2d[]{
-                            new Point2d(-16, 19),
+                            new Point2d(-16, -19),
                             //new Point2d(0,30),
                             //new Point2d(6.5, 48)
-                            new Point2d(13, 49)
+                            new Point2d(13, -49)
                     }, 2000)
                     .waitMilliseconds(1000)
                     .callback(() -> {
@@ -103,10 +99,10 @@ public class PPCloseRedAuto extends BluLinearOpMode {
 
                     // HEAD BACK
                     .addPurePursuitPath(new Point2d[]{
-                            new Point2d(6.5, 54),   // was (12.5, 46)
-                            new Point2d(-16, 19)    // was (-10, 17)
+                            new Point2d(6.5, -54),   // was (12.5, 46)
+                            new Point2d(-16, -19)    // was (-10, 17)
                     }, 2000)
-                    .addTurnTo(45,1000)
+                    .addTurnTo(-45,1000)
                     .waitMilliseconds(500)
                     .callback(()->{
                         new TurnTurretToPosFieldCentricCommand(turretAngle).schedule();
@@ -120,13 +116,13 @@ public class PPCloseRedAuto extends BluLinearOpMode {
                         ).schedule();
                     })
                     .waitMilliseconds(200)
-                    .addTurnTo(35,500)
+                    .addTurnTo(-35,500)
 
                     // PICKUP THIRD SET
                     .addTurnTo(45, 1000)
                     .addPurePursuitPath(new Point2d[]{
-                            new Point2d(-16, 19),   // was (-10, 17)
-                            new Point2d(36, 51)     // was (37, 46)
+                            new Point2d(-16, -19),   // was (-10, 17)
+                            new Point2d(39, -46)     // was (37, 46)
                     }, 1100)
                     .waitMilliseconds(1000)
                     .callback(() -> {
@@ -137,12 +133,12 @@ public class PPCloseRedAuto extends BluLinearOpMode {
                     })
                     .waitMilliseconds(1000)
                     .addPurePursuitPath(new Point2d[]{
-                            new Point2d(36, 48),
+                            new Point2d(36, -48),
 //                            new Point2d(0,25),
-                            new Point2d(-16, 19)    // was (-10, 17)
+                            new Point2d(-16, -19)    // was (-10, 17)
                     }, 2000)
 //                    .waitMilliseconds(1000)
-                    .addTurnTo(45,1000)
+                    .addTurnTo(-45,1000)
                     .waitMilliseconds(500)
                     .callback(()->{
                         new TurnTurretToPosFieldCentricCommand(turretAngle).schedule();
@@ -157,8 +153,8 @@ public class PPCloseRedAuto extends BluLinearOpMode {
                     })
                     .waitMilliseconds(300)
                     .addPurePursuitPath(new Point2d[]{
-                            new Point2d(-16, 19),    // was (37, 46)
-                            new Point2d(0, 30)    // was (-10, 17)
+                            new Point2d(-16, -19),    // was (37, 46)
+                            new Point2d(0, -30)    // was (-10, 17)
                     }, 1300)
                     // SHOOT THIRD SET
                     .build();
@@ -193,9 +189,9 @@ public class PPCloseRedAuto extends BluLinearOpMode {
 
     public void onStart() {
         shooter.shootWithVelocity(1120); // orig 850 before switching to triple shot
-        turret.setAngle(-5);
+        turret.setAngle(5);
         llTagDetector.switchToMotif();
-        sixWheel.setPosition(new Pose2d(-51, 54, Math.toRadians(-51.529)));
+        sixWheel.setPosition(new Pose2d(-49, -54, Math.toRadians(51.529)));
         currentPath = new TestingPath().build().start();
     }
 
