8e88b36bf5561a681076355cf650101752e39821
Alex <47759254+acurra3398@users.noreply.github.com>
2026-02-28 15:19:53 -0500

    Update farBLUEautoFSMwithBallDetection.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/auto/farBLUEautoFSMwithBallDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/auto/farBLUEautoFSMwithBallDetection.java
@@ -237,6 +237,8 @@ public class farBLUEautoFSMwithBallDetection extends BluLinearOpMode {
             double minX = 20; // x value the closest we would ever want to intake towards the gate
             double maxX = 62; // max x value we would want to intake towards the wall
             pickupWallX = Range.clip(fieldX, minX, maxX);  //limits the x value from which we intake to a set range
+        } else {
+            pickupWallX = null;
         }
     }
 
