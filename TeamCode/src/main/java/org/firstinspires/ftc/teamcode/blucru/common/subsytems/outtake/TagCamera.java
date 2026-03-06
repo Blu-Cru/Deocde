464d541e91cb8dfbcd665c0f0e3d3b3dabd65196
Deven Balaji <devenvbalaji@gmail.com>
2026-03-06 15:16:50 -0500

    converting stuff to vectors for cleaner code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/outtake/TagCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/outtake/TagCamera.java
@@ -11,6 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.R;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.BluSubsystem;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.outtake.shooter.ShooterAutoAimInterpolation;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.outtake.shooter.ShooterMotifCoordinator;
@@ -18,6 +19,7 @@ import org.firstinspires.ftc.teamcode.blucru.common.util.Alliance;
 import org.firstinspires.ftc.teamcode.blucru.common.util.Globals;
 import org.firstinspires.ftc.teamcode.blucru.common.util.MotifPattern;
 import org.firstinspires.ftc.teamcode.blucru.common.util.Pose2d;
+import org.firstinspires.ftc.teamcode.blucru.common.util.Vector2d;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -32,6 +34,7 @@ public class TagCamera implements BluSubsystem, Subsystem {
     boolean currentlySeeingGoodTags;
     boolean streaming;
     final double tagDistToMiddleShooter = 8;
+    final double turretCenterToLocPoint = -72.35/25.4;
     MotifPattern motifPattern;
 
 
@@ -95,11 +98,27 @@ public class TagCamera implements BluSubsystem, Subsystem {
                 }
                 double tagFieldX = detection.metadata.fieldPosition.get(0);
                 double tagFieldY = detection.metadata.fieldPosition.get(1);
-                double cameraFieldHeading = Robot.getInstance().sixWheelDrivetrain.getPos().getH() + Robot.getInstance().turret.getAngle();
+                Vector2d originToTag = new Vector2d(tagFieldX, tagFieldY);
+                double cameraFieldHeading = Robot.getInstance().sixWheelDrivetrain.getPos().getH() + Math.toRadians(Robot.getInstance().turret.getAngle());
                 double angle = cameraFieldHeading;
                 double dx = detection.ftcPose.x; // left/right relative to camera
                 double dy = detection.ftcPose.y; // forward/back relative to camera
-                double camToTagFieldX = dx * Math.cos(angle) - dy * Math.sin(angle);
+                Vector2d tagToCamCamCentric = new Vector2d(dx, dy);
+                Vector2d tagToCam = tagToCamCamCentric.rotate(angle);
+
+                Vector2d camToTurret = new Vector2d(tagDistToMiddleShooter,0).rotate(cameraFieldHeading);
+
+                Vector2d turretToRobot = new Vector2d(turretCenterToLocPoint, 0).rotate(Robot.getInstance().sixWheelDrivetrain.getPos().getH());
+
+                Vector2d tagToRobot = tagToCam.addNotInPlace(camToTurret).addNotInPlace(turretToRobot);
+
+                Vector2d originToRobot = originToTag.addNotInPlace(tagToRobot);
+
+                Robot.getInstance().sixWheelDrivetrain.setPosition(new Pose2d(originToRobot, Robot.getInstance().sixWheelDrivetrain.getPos().getH()));
+
+
+                //non-vector code
+                /*double camToTagFieldX = dx * Math.cos(angle) - dy * Math.sin(angle);
                 double camToTagFieldY = dx * Math.sin(angle) + dy * Math.cos(angle);
                 double cameraFieldX = tagFieldX - camToTagFieldX;
                 double cameraFieldY = tagFieldY - camToTagFieldY;
@@ -110,7 +129,7 @@ public class TagCamera implements BluSubsystem, Subsystem {
                 double robotFieldX = cameraFieldX - rotatedOffsetX;
                 double robotFieldY = cameraFieldY - rotatedOffsetY;
                 Robot.getInstance().sixWheelDrivetrain.setPosition(new Pose2d(robotFieldX, robotFieldY, Robot.getInstance().sixWheelDrivetrain.getPos().getH())
-                );
+                );*/
             }
 
         }
