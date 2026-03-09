f9c091e2511f7ee0c1a2d225167b7984d4b689dd
Deven Balaji <devenvbalaji@gmail.com>
2026-03-08 22:38:00 -0400

    cam test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmodes/test/TurretCamTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.blucru.opmodes.test;
+
+import org.firstinspires.ftc.teamcode.blucru.common.hardware.motor.BluEncoder;
+import org.firstinspires.ftc.teamcode.blucru.common.subsytems.Robot;
+import org.firstinspires.ftc.teamcode.blucru.opmodes.BluLinearOpMode;
+
+public class TurretCamTest extends BluLinearOpMode {
+    public void initialize(){
+        robot.clear();
+        robot.addTurretCam();
+        addSixWheel();
+        addTurret();
+    }
+
+    public void periodic(){
+        telemetry.addData("Robot Pos Cam", Robot.getInstance().turretCam.getBotPosePoseHistory());
+        telemetry.addData("Robot Pos Loc", sixWheel.getPos())''
+    }
+}
