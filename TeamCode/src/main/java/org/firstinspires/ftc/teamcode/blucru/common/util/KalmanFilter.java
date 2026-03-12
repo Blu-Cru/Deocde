2a8c567db04106e8064e1d450b7cdbc5e6fa8fa8
Alex <47759254+acurra3398@users.noreply.github.com>
2026-03-11 22:49:31 -0400

    Fixed Variable Names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/util/KalmanFilter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/util/KalmanFilter.java
@@ -4,37 +4,39 @@ public class KalmanFilter {
     static double x = 0; // your initial state
     static double Q = 0.1; // your model covariance
     static double R = 0.4; // your sensor covariance
-    static double p = 1; // your initial covariance guess
+    static double P = 1; // your initial covariance guess
     static double K = 1; // your initial Kalman gain guess
 
     static double x_previous = x;
-    static double p_previous = p;
+    static double p_previous = P;
     static double u = 0;
     static double z = 0;
     static double lastinput = 0;
 
-    public KalmanFilter(double x, double Q, double R, double p, double K) {
+    public KalmanFilter(double x, double Q, double R, double P, double K) {
         this.x = x;
         this.Q = Q;
         this.R = R;
+        this.P = P;
+        this.K = K;
     }
     public void update(double input) {
         double predictedinput = input-lastinput;
         u = predictedinput; // The delta
         x = x_previous + u;
 
-        p = p_previous + Q;
+        P = p_previous + Q;
 
-        K = p/(p + R);
+        K = P/(P + R);
 
         z = input; // The known current input
 
         x = x + K * (z - x);
 
-        p = (1 - K) * p;
+        P = (1 - K) * P;
 
         x_previous = x;
-        p_previous = p;
+        p_previous = P;
         lastinput = input;
     }
 
@@ -42,6 +44,6 @@ public class KalmanFilter {
         return x;
     }
     public double getUncertainty() {
-        return p;
+        return P;
     }
 }
