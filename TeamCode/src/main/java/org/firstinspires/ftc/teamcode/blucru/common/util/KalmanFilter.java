e85df3cb72c50bb02b9a4e292c973bee816b20d2
Alex <47759254+acurra3398@users.noreply.github.com>
2026-03-12 17:38:27 -0400

    Added Manual Last input for loc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/util/KalmanFilter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/util/KalmanFilter.java
@@ -40,6 +40,26 @@ public class KalmanFilter {
         lastinput = input;
     }
 
+    public void update(double input, double givenlastinput) {
+        double predictedinput = input-givenlastinput;
+        u = predictedinput; // The delta
+        x = x_previous + u;
+
+        P = p_previous + Q;
+
+        K = P/(P + R);
+
+        z = input; // The known current input
+
+        x = x + K * (z - x);
+
+        P = (1 - K) * P;
+
+        x_previous = x;
+        p_previous = P;
+        lastinput = input;
+    }
+
     public double get() {
         return x;
     }
