eb2f5f52818b3db0fdcb2646ec0ac14b5630ea26
Deven Balaji <devenvbalaji@gmail.com>
2026-03-27 16:35:31 -0400

    debug telem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/outtake/turret/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/outtake/turret/Turret.java
@@ -97,6 +97,7 @@ public class Turret implements BluSubsystem, Subsystem {
                 //Globals.telemetry.addData("Turret Cam Detecting", Robot.getInstance().turretCam.detectedThisLoop());
                 Globals.telemetry.addData("Turret Offset", headingOffset);
                 if (Robot.getInstance().turretCam.getDetection() != null && ( Robot.getInstance().turretCam.detectedThisLoop() || (Robot.getInstance().turretCam.getDetection().frameAcquisitionNanoTime) < 2000000)){
+                    Globals.telemetry.addLine("here hallo hi hi hi hi hallo here");
                     tagBasedAutoAim(Robot.getInstance().turretCam.getDetection());
                     if (lastAutoAimMode == LastAutoAimMode.LOC){
                         Globals.telemetry.addLine("SWITCHING TO CAMERA");
