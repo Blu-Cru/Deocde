1499762589291e4bfcf64119d10daa174956c4d7
Alex <47759254+acurra3398@users.noreply.github.com>
2026-04-01 11:28:37 -0400

    Servo Voltage Scalling!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/hardware/servo/BluCRServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/hardware/servo/BluCRServo.java
@@ -41,9 +41,10 @@ public class BluCRServo extends CRServoImpl implements BluHardwareDevice {
 
     @Override
     public void write() {
-        if (Math.abs(power - lastPower) > 0.005){
-            lastPower = power;
-            super.setPower(power);
+        double correctedPower = Globals.getCorrectPower(power);
+        if (Math.abs(correctedPower - lastPower) > 0.005){
+            lastPower = correctedPower;
+            super.setPower(correctedPower);
         }
     }
 
