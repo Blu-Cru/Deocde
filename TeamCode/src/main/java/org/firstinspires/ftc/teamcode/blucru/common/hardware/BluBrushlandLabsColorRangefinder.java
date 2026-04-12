38acfb714830d1f7c0e1d8d6af567fb6af1df8ed
Alex <47759254+acurra3398@users.noreply.github.com>
2026-04-12 18:14:03 -0400

    Add analog support to color rangefinder
    Add AnalogInput support and voltage-based color detection to BluBrushlandLabsColorRangefinder. Introduce voltage threshold constants, a useAnalog flag, and a new constructor overload that accepts an analog pin. Read and expose voltage, add isGreen/isPurple/hasArtifact/getVoltage helpers, and make ball detection use analog artifact/voltage when available while preserving digital-only behavior for existing callers.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/hardware/BluBrushlandLabsColorRangefinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/hardware/BluBrushlandLabsColorRangefinder.java
@@ -1,18 +1,36 @@
 package org.firstinspires.ftc.teamcode.blucru.common.hardware;
 
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 import org.firstinspires.ftc.teamcode.blucru.common.util.Globals;
 
 public class BluBrushlandLabsColorRangefinder implements BluHardwareDevice{
 
+    public static final double GREEN_VOLTAGE_LOW = 1.2;
+    public static final double GREEN_VOLTAGE_HIGH = 1.9;
+    public static final double PURPLE_VOLTAGE_HIGH = 1.2;
+    public static final double DISTANCE_THRESHOLD_MM = 60.0;
+
     private DigitalChannel pin0, pin1;
+    private AnalogInput analogPin0;
     private boolean state1;
     private boolean state2;
+    private double voltage;
+    private boolean useAnalog;
 
     public BluBrushlandLabsColorRangefinder(String pin0, String pin1){
         this.pin0 = Globals.hwMap.get(DigitalChannel.class,pin0);
         this.pin1 = Globals.hwMap.get(DigitalChannel.class,pin1);
+        this.useAnalog = false;
+        configurePinsForInput();
+    }
+
+    public BluBrushlandLabsColorRangefinder(String pin0, String pin1, String analogPinName){
+        this.pin0 = Globals.hwMap.get(DigitalChannel.class,pin0);
+        this.pin1 = Globals.hwMap.get(DigitalChannel.class,pin1);
+        this.analogPin0 = Globals.hwMap.get(AnalogInput.class, analogPinName);
+        this.useAnalog = true;
         configurePinsForInput();
     }
 
@@ -25,24 +43,56 @@ public class BluBrushlandLabsColorRangefinder implements BluHardwareDevice{
     public void read() {
         state1 = pin0.getState();
         state2 = pin1.getState();
+        if (useAnalog) {
+            voltage = analogPin0.getVoltage();
+        }
     }
 
     @Override
     public void write() {
     }
 
-    public boolean ballDetected(){
-        return state1 || state2;
+    public double getVoltage() {
+        return voltage;
     }
-    public boolean greenBall(){
+
+    public boolean hasArtifact() {
         return state2;
     }
-    public boolean purpleBall(){
+
+    public boolean isGreen() {
+        if (useAnalog) {
+            return hasArtifact() && voltage > GREEN_VOLTAGE_LOW && voltage < GREEN_VOLTAGE_HIGH;
+        }
+        return state2;
+    }
+
+    public boolean isPurple() {
+        if (useAnalog) {
+            return hasArtifact() && voltage < PURPLE_VOLTAGE_HIGH;
+        }
         return state1;
     }
+
+    public boolean ballDetected(){
+        if (useAnalog) {
+            return hasArtifact();
+        }
+        return state1 || state2;
+    }
+
+    public boolean greenBall(){
+        return isGreen();
+    }
+
+    public boolean purpleBall(){
+        return isPurple();
+    }
+
     public boolean getRawState1(){
         return state1;
     }
+
     public boolean getRawState2(){
         return state2;
     }
