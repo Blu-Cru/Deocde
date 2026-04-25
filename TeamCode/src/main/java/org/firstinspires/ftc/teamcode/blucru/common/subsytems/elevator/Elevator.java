3dc77e2ac9926c0f3a8d91b29e9c28c8b2456b2f
Alex <acurra3398@gmail.com>
2026-04-25 18:54:01 -0400

    Add elevator telemetry and import Globals
    Add a Globals import and update Elevator.telemetry() to report the elevator fullness via telemetry.addData("Elevator Full?", isFull()). Removed previously commented-out servo telemetry calls so the telemetry now surfaces the elevator occupancy state for easier debugging.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/elevator/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/elevator/Elevator.java
@@ -9,6 +9,8 @@ import org.firstinspires.ftc.teamcode.blucru.common.hardware.servo.BluServo;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.BluSubsystem;
 import org.firstinspires.ftc.teamcode.blucru.common.subsytems.outtake.shooter.ShooterMotifCoordinator;
 import org.firstinspires.ftc.teamcode.blucru.common.util.BallColor;
+import org.firstinspires.ftc.teamcode.blucru.common.util.Globals;
+
 @Config
 public class Elevator implements BluSubsystem, Subsystem {
     private BluServo elevatorServoLeft;
@@ -162,8 +164,7 @@ public class Elevator implements BluSubsystem, Subsystem {
 
     @Override
     public void telemetry(Telemetry telemetry) {
-        //elevatorServoLeft.telemetry();
-        //elevatorServoRight.telemetry();
+        telemetry.addData("Elevator Full?", isFull());
     }
 
     @Override
